4954baeea21a0b670e8ab2669a404963c18ff891
dakerdenis <dakerdenis@gmail.com>
2025-08-19 10:25:05 +0400

    added

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -20,10 +20,10 @@ class ModelTests(TestCase):
         
     def test_new_user_email_normalized(self):
         sample_emails = [
-            ['test1@EXAMPLE.com','test1@example.com'],
-            ['Test2@Eample.com','Test2@example.com'],
-            ['TEST3@EXAMPLE.COM','Test3@example.com'],
-            ['test4@example.com','test4@example.com'],
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
         ]
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
@@ -33,3 +33,12 @@ class ModelTests(TestCase):
         """Test that creating a user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
+            
+    def test_create_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123'
+        )
+        
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
